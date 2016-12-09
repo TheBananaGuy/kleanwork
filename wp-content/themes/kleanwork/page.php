5426dc26d6f66dffaaa39f7aa0f9c73ff75c2856
Aleksei <our.denouement@gmail.com>
2016-12-09 11:33:40 +0100

    page builder + custom fields installed, gulp task for sass & js configured

--- wp-content/themes/kleanwork/page.php
+++ wp-content/themes/kleanwork/page.php
@@ -8,6 +8,7 @@
 <section class="entry-content">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
+<?php // if(the_field('bonus_area')) {the_field('bonus_area');} ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
 </article>
