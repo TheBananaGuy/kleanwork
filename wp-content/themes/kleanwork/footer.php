b79ab83a132d77f2534e3fed365f8066a9ade16f
Aleksei <our.denouement@gmail.com>
2016-12-12 14:39:41 +0100

    klean.dk styles imported

--- wp-content/themes/kleanwork/footer.php
+++ wp-content/themes/kleanwork/footer.php
@@ -3,7 +3,7 @@
 			</div>
 			<footer id="footer" role="contentinfo">
 				<div id="copyright">
-				<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'kleanwork' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'kleanwork' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
+				<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights are going to be Reserved.', 'kleanwork' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'kleanwork' ), '<a href="http://klean.dk/">Klean</a>' ); ?>
 				
 				</div>
 			</footer>
