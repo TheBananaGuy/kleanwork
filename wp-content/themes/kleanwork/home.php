a262b5f78777cdb16d05b65600c93c478f50c925
Aleksei <our.denouement@gmail.com>
2016-12-14 11:43:26 +0100

    removed unneccessary files, added custom template files

--- wp-content/themes/kleanwork/home.php
+++ wp-content/themes/kleanwork/home.php
@@ -2,6 +2,10 @@
 
 			<section id="content" role="main">
 
+<?php
+$page_for_posts = get_option('page_for_posts');
+if(the_field('needs_main_content', $page_for_posts)) {the_field('needs_main_content', $page_for_posts);} ?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -11,13 +15,11 @@
 <?php// edit_post_link(); ?>
 
 					</header>
-					<section class="entry-content">
+						<section class="entry-content">
 
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 
 <?php the_content(); ?>
-
-<?php // if(the_field('bonus_area')) {the_field('bonus_area');} ?>
 
 						<div class="entry-links"><?php wp_link_pages(); ?></div>
 					</section>
--- wp-content/themes/kleanwork/single.php
+++ wp-content/themes/kleanwork/single.php
@@ -4,7 +4,7 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
-<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
+<?php // if ( ! post_password_required() ) comments_template( '', true ); ?>
 <?php endwhile; endif; ?>
 
 				<footer class="footer">
@@ -14,5 +14,5 @@
 				</footer>
 			</section>
 
-<?php get_sidebar(); ?>
+<?php // get_sidebar(); ?>
 <?php get_footer(); ?>
