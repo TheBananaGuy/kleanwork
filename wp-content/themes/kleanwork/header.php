a78c1707ae8a8fc9ac5182b5d50ed33c366a3af4
Aleksei <our.denouement@gmail.com>
2016-12-16 03:16:48 +0100

    removed bloat, added dynamic meta description

--- wp-content/themes/kleanwork/header.php
+++ wp-content/themes/kleanwork/header.php
@@ -5,7 +5,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-
+		<meta description="<?php bloginfo( 'description' ); ?>">
 <?php wp_enqueue_script("jquery"); ?>
 <?php wp_head(); ?>
 		
@@ -15,59 +15,46 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+
 		<div id="wrapper" class="hfeed">
 			<header id="header" role="banner">
-
-		        <div id="pseudo-header">
-		            <div class="header">
-		                <a href="<?php print esc_url( home_url( '/' )); ?>"><img src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" data-src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" height="66" width="462" alt="Logo" class="logo b-lazy" /></a>
-		                <div class="social js-open-social">
-		                    <p><span class="icon icon-some"></span></p>
-		                </div>
-		                <div class="menu js-open-menu">
-		                    <p>
-		                        <span class="icon icon-menu">
-		                            Menu
-		                        </span>
-		                    </p>
-		                </div>
-		            </div>
-		        </div>
-
-<?php wp_custom_menu(); ?>
-
-		        <div id="header-container" class="header__outer stick">
-
-<?php wp_custom_menu(); ?>
-
-		            <div class="header">
-	                    <a href="<?php print esc_url( home_url( '/' )); ?>"><img class="b-lazy logo" src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" data-src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" height="66" width="462" alt="Logo" /></a>
-		                <div class="social js-open-social">
-		                    <p><span class="icon icon-some"></span></p>
-		                </div>
-		                <div class="menu js-open-menu">
-		                    <p>
-		                        <span class="icon icon-menu">
-		                            Menu
-		                        </span>
-		                    </p>
-		                </div>
-		            </div>
-		        </div>
-			
-				<section id="branding">
-					<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-					<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-				</section>
 				<nav id="menu" role="navigation">
-					<div id="search">
+			        <div id="pseudo-header">
+			            <div class="header">
+			                <a href="<?php print esc_url( home_url( '/' )); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><img src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" data-src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" height="66" width="462" alt="Logo" class="logo b-lazy" /></a>
+			                <div class="social js-open-social">
+			                    <p><span class="icon icon-some"></span></p>
+			                </div>
+			                <div class="menu js-open-menu">
+			                    <p>
+			                        <span class="icon icon-menu">
+			                            Menu
+			                        </span>
+			                    </p>
+			                </div>
+			            </div>
+			        </div>
 
-<?php // get_search_form(); ?>
+<?php wp_custom_menu(); ?>
 
-					</div>
+		      	  	<div id="header-container" class="header__outer stick">
 
+<?php wp_custom_menu(); ?>
 
-
-				</nav>
+			            <div class="header">
+		                    <a href="<?php print esc_url( home_url( '/' )); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><img class="b-lazy logo" src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" data-src="<?php print_source(IMAGE_FOLDER); ?>logo.svg" height="66" width="462" alt="Logo" /></a>
+			                <div class="social js-open-social">
+			                    <p><span class="icon icon-some"></span></p>
+			                </div>
+			                <div class="menu js-open-menu">
+			                    <p>
+			                        <span class="icon icon-menu">
+			                            Menu
+			                        </span>
+			                    </p>
+			                </div>
+			            </div>
+			        </div>
+		        </nav>
 			</header>
 			<div id="container">
