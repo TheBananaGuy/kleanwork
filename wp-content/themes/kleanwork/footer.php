2d3c5caaa1319436f4dc915e88390c5026260b5a
Aleksei <our.denouement@gmail.com>
2016-12-19 05:56:35 +0100

    footer information corrections

--- wp-content/themes/kleanwork/footer.php
+++ wp-content/themes/kleanwork/footer.php
@@ -2,9 +2,9 @@
 		    <footer class="band band--banner band--dark b-lazy-bg">
 		        <div class="wrap">
 
-		            <h2>FANG OS P&#197;</h2>
+		            <h2>GET IN TOUCH</h2>
 
-		                    <p class="contact-phone"><a href="tel:70260727"><span>70 260 727</span></a></p>
+		                    <p class="contact-phone"><a href="tel:70202797"><span>70 202 797</span></a></p>
 
 		            <div class="some">
 		                    <div class="some__icon">
@@ -24,21 +24,20 @@
 		                    </div>
 		            </div>
 
-		            <p class="manchet">Vil du holde dig opdateret med de nyeste trends - og hvad vi g&#229;r og pusler med? S&#229; tilmeld dig vores nyhedsbrev.</p>
+		            <p class="manchet">Want to keep up to date with the latest trends and what we are puttering with? <br/>Sign up for our newsletter</p>
 
 	                <form method="post" action="http://client2.mailmailmail.net/form.php?form=1486" data-invalid-email="Ugyldig email adresse" data-required-email="Feltet er p&#229;kr&#230;vet" onsubmit=" return CheckForm(this); " class="newsletter newsletter-subscription-form js-newsletter-form">
 
 	                    <input type="email" name="email" class="mod--cutoff-right" required placeholder="example@mail.com" />
-	                    <button type="submit" class="button button--secondary mod--cutoff-left">Tilmeld</button>
+	                    <button type="submit" class="button button--secondary mod--cutoff-left">Subscribe</button>
 
 	                </form>
-
 	                <p class="small">
-	                    <span class="palm-break"><strong class="line-spacing--right">727</strong>Niels Bohrs Vej 23, Stilling</span>
+	                    <span class="palm-break"><strong class="line-spacing--right">Klean</strong>Skovvejen 2A</span>
 	                    <span class="line-spacing is-hidden--palm">|</span>
-	                    DK-8660 Skanderborg
+	                    DK-8000 Aarhus
 	                    <span class="line-spacing">|</span>
-	                    <a href="tel:70260727">70 260 727</a>
+	                    <a href="tel:70202797">70 202 797</a>
 	                </p>
 	                <p class="small">
 	                    <span class="palm-break"><strong class="line-spacing--right">Klean</strong>Bredgade 23B</span>
@@ -48,12 +47,13 @@
 	                    <a href="tel:70202797">70 202 797</a>
 	                </p>
 	                <p class="small">
-	                    <span class="palm-break"><strong class="line-spacing--right">Klean</strong>Skovvejen 2A</span>
+	                    <span class="palm-break"><strong class="line-spacing--right">727</strong>Niels Bohrs Vej 23, Stilling</span>
 	                    <span class="line-spacing is-hidden--palm">|</span>
-	                    DK-8000 Aarhus
+	                    DK-8660 Skanderborg
 	                    <span class="line-spacing">|</span>
-	                    <a href="tel:70202797">70 202 797</a>
+	                    <a href="tel:70260727">70 260 727</a>
 	                </p>
+
 		        </div>
 		    </footer>
 		</div>
