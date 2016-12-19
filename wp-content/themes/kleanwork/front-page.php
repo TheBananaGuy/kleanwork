57a1f2253dfced0652d68bb7449f9dad2402d4e0
Aleksei <our.denouement@gmail.com>
2016-12-19 05:37:16 +0100

    main page thumbnail properly showing up

--- wp-content/themes/kleanwork/front-page.php
+++ wp-content/themes/kleanwork/front-page.php
@@ -5,15 +5,22 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="header">
-					<h1 class="entry-title"><?php the_title(); ?></h1>
+					<header class="js-top-banner">
+						<div class="band band--banner band--dark js-banner-picture band--picture js-show-picture-banner b-lazy-bg" <?php if ( has_post_thumbnail() ) { print('style="background-image:url(' . get_the_post_thumbnail_url() . ');"'); } ?> >
+							<div class="wrap">
+								<h1 class="entry-title"><?php the_title(); ?></h1>
+							</div>
+							
+						</div>
+					</header>
+
+					
 
 <?php // edit_post_link(); ?>
 
-					</header>
 					<section class="entry-content">
 
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+<?php // if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 
 						<div class="band">
 							<div class="wrap">
