1b73136028403f0f6b45d9871e8a560c318b99b7
Sam Mullen <our.denouement@gmail.com>
2017-01-12 14:09:13 +0100

    application form, visual

--- wp-content/themes/kleanwork/page-apply.php
+++ wp-content/themes/kleanwork/page-apply.php
@@ -21,13 +21,15 @@
 
 <?php the_content(); ?>
 
-										<form>
-											<input class="float_input" required="" type="text" name="apply_name">
-											<select required="">
+										<form class="js-form" name="application-form" method="POST" action="" novalidate >
 
-												<option>Position</option>
+											<div class="grid-group">
+												<div class="grid size-12">
+														<select required="">
 
-												<option>---</option>
+															<option>What are you applying for?</option>
+
+															<option>---</option>
 <?php
 // global $post;
 $child_pages_query_args = array(
@@ -38,7 +40,7 @@ $child_pages = new WP_Query( $child_pages_query_args );
 
 if ( $child_pages->have_posts() ) : while ( $child_pages->have_posts() ) : $child_pages->the_post();
 print ('
-												<option>'); the_title(); print('</option>
+															<option>'); the_title(); print('</option>
 ');
 
 endwhile; endif;
@@ -47,14 +49,53 @@ endwhile; endif;
 wp_reset_postdata();
 ?>
 
-												<option>---</option>
+															<option>---</option>
 
-												<option>Unsolicited application</option>
+															<option>Unsolicited application</option>
 
-											</select>
-											<input class="float_input" required="" type="email" name="apply_email">
-											<input class="float_input" required="" type="tel" name="apply_phone">
+														</select>
+												</div>
+											</div>
+
+											<div class="grid-group">
+												<div class="grid size-12">
+													<div class="float js-float float--label-hidden">
+														<label class="float__label">Name</label>
+														<input class="float__input" type="text" name="apply_name" placeholder="Name" data-hint="Your phone name" data-msg="Please enter name" required >
+													</div>
+												</div>
+											</div>
+
+											<div class="grid-group">
+												<div class="grid size-12">
+													<div class="float js-float float--label-hidden">
+														<label class="float__label">E-mail</label>
+														<input class="float__input" type="email" name="apply_mail" placeholder="e-mail" data-hint="Your e-mail address" data-msg="Please enter a valid e-mail" data-rule-email="true" required >
+													</div>
+												</div>
+											</div>
+
+											<div class="grid-group">
+												<div class="grid size-12">
+													<div class="float js-float float--label-hidden">
+														<label class="float__label">Phone number</label>
+														<input class="float__input" type="tel" name="apply_phone" placeholder="Phone number" data-hint="Your phone number" data-msg="Please enter a valid phone number" required >
+													</div>
+												</div>
+											</div>
+
+											<div class="grid-group">
+												<div class="grid size-12">
+													<div class="float js-float float--label-hidden">
+														<label class="float__label">Application attachement</label>
+														<input class="float__input file__dupe" type="text" name="apply_dummy" placeholder="Application attachement" required >
+														<input class="float__input file__ghost" type="file" name="apply_file" data-hint="Your supporting materials (CV, portfolio, etc)" data-msg="Please attach a valid document" required >
+													</div>
+												</div>
+											</div>
+
 											<input class="right" type="submit" name="apply_apply" value="Apply now!">
+
 										</form>
 
 									</div>
